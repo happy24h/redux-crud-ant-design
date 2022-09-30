6aa14831b29b59ce74b64bb5982fccd9efc20e4f
@anhnv99 <vietanhhappy99@gmail.com>
2022-09-30 09:13:12 +0700

    update

--- src/pages/EditUser.js
+++ src/pages/EditUser.js
@@ -111,7 +111,7 @@ function EditUser() {
           htmlType="submit"
           onClick={handleSubmit}
         >
-          Add User
+          Update
         </Button>
       </Form>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { listUsers, userDelete } from "../redux/actions";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+// import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 function Home() {
   let dispatch = useDispatch();
@@ -36,17 +36,16 @@ function Home() {
       key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <Button type="primary" ghost onClick={() => handleEditUser(record)}>
-            <EditOutlined />
+          <Button type="primary" onClick={() => handleEditUser(record)}>
+            {/* <EditOutlined /> */}
             Edit
           </Button>
           <Button
             type="primary"
             danger
-            ghost
             onClick={() => handleDeleteUser(record)}
           >
-            <DeleteOutlined />
+            {/* <DeleteOutlined /> */}
             Delete
           </Button>
         </Space>
